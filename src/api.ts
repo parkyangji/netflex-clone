b71fc423338a23f6d727494067bfa9e2f68a7dd3
parkyangji <dhsfid02@gmail.com>
2023-11-17 23:21:42 +0900

    api 재조정

--- src/api.ts
+++ src/api.ts
@@ -21,8 +21,23 @@ export interface IGetMoviesResult {
 }
 
 // fetch는 데이터를 받아오고 json을 리턴하는 함수
+/*
 export function getMovies() {
   return fetch(`${BASE_PATH}/movie/now_playing?api_key=${API_KEY}`).then(
     (response) => response.json()
   );
+}
+*/
+const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMjYwMmE2YjQ4ZWUxMzcwY2Y4MWJiMTIzMjQ4MTA5MCIsInN1YiI6IjY1NGNiZjM4NDFhNTYxMzM2ODg2NTQ3ZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.7S2Kk5fELxQWLtV7aNYdrqTHbm3nK7qWgw8V-o4Jro0'
+  }
+};
+
+export function getMovies(){
+  return fetch(`${BASE_PATH}/movie/now_playing?language=ko-KR&page=1`, options).then(
+    response => response.json()
+    )
 }
