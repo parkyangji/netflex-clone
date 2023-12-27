fba1009bd4f906e60351aa0b1d11cdf5c964cb6e
parkyangji <dhsfid02@gmail.com>
2023-12-27 16:19:09 +0900

    GlobalStyle 이동, 모바일 화면 구분

--- src/App.tsx
+++ src/App.tsx
@@ -1,87 +1,41 @@
 import { Outlet } from "react-router-dom";
 import Header from "./Components/Header";
 import Footer from "./Components/Footer"
-import Home from "./Routes/Home";
 import { createGlobalStyle } from "styled-components";
 import { Helmet } from "react-helmet-async";
+import { useMediaQuery } from "react-responsive";
+import MobileHeader from "./Components/MobileHeader";
+import { responsiveSize } from "./theme";
 
-const GlobalStyle = createGlobalStyle`
-html, body, div, span, applet, object, iframe,
-h1, h2, h3, h4, h5, h6, p, blockquote, pre,
-a, abbr, acronym, address, big, cite, code,
-del, dfn, em, img, ins, kbd, q, s, samp,
-small, strike, strong, sub, sup, tt, var,
-b, u, i, center,
-dl, dt, dd, menu, ol, ul, li,
-fieldset, form, label, legend,
-table, caption, tbody, tfoot, thead, tr, th, td,
-article, aside, canvas, details, embed,
-figure, figcaption, footer, header, hgroup,
-main, menu, nav, output, ruby, section, summary,
-time, mark, audio, video {
-  margin: 0;
-  padding: 0;
-  border: 0;
-  font-size: 100%;
-  font: inherit;
-  vertical-align: baseline;
-}
-/* HTML5 display-role reset for older browsers */
-article, aside, details, figcaption, figure,
-footer, header, hgroup, main, menu, nav, section {
-  display: block;
-}
-/* HTML5 hidden-attribute fix for newer browsers */
-*[hidden] {
-    display: none;
-}
-body {
-  line-height: 1;
-}
-menu, ol, ul {
-  list-style: none;
-}
-blockquote, q {
-  quotes: none;
-}
-blockquote:before, blockquote:after,
-q:before, q:after {
-  content: '';
-  content: none;
-}
-table {
-  border-collapse: collapse;
-  border-spacing: 0;
-}
-* {
-  box-sizing: border-box;
-}
-body {
-  font-weight: 300;
-  font-family: 'Noto Sans KR', sans-serif;
-  color: ${props => props.theme.white.darker};
-  line-height: 1.2;
-  background-color: black;
-  overflow-x: hidden;
-}
-a {
-  text-decoration:none;
-  color:inherit;
-}
-`;
 
 function App(){
-  return (
-    <>
-      <Helmet>
-        <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400&display=swap" rel="stylesheet" />
-      </Helmet>
-      <GlobalStyle/>
-      <Header/>
-      <Outlet/>
-      <Footer/>
-    </>
-  )
+  const isMobile = useMediaQuery({
+    query: `(max-width : ${responsiveSize.mobile})`
+  })
+
+  if (isMobile) {
+    return (
+      <>
+        <Helmet>
+          <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400&display=swap" rel="stylesheet" />
+        </Helmet>
+        <MobileHeader/>
+        <Outlet/>
+      </>
+    )
+  } else {
+    return (
+      <>
+        <Helmet>
+          <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400&display=swap" rel="stylesheet" />
+        </Helmet>
+        <Header/>
+        <Outlet/> 
+        <Footer/>
+      </>
+    )
+  }
+  // <Outlet/> => index.tsx에 router
 }
 
 export default App;
