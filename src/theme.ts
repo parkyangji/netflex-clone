35553fd3c05d4858c8d52f89e7b308e92c5d323d
parkyangji <dhsfid02@gmail.com>
2023-12-30 15:11:26 +0900

    모바일, pc 화면 재조정

--- src/theme.ts
+++ src/theme.ts
@@ -1,13 +1,13 @@
 import { DefaultTheme } from "styled-components";
 
 export const isPcCheck = {
-  query: `(min-width : 1024px)`
+  query: `(min-width : 1025px)`
 }
 export const isTabletCheck = {
-  query: `(min-width : 768px) and (max-width : 1023px)`
+  query: `(min-width : 769px) and (max-width : 1024px)`
 }
 export const isMobileCheck = {
-  query: `(max-width : 767px)`
+  query: `(max-width : 768px)`
 }
 
 export const responsiveSize = {
